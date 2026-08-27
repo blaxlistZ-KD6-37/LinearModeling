5c90cf6c1501075b857501e879deac6da9f79ce4
blaxlistZ <blaxlistz@gmail.com>
2026-08-27 20:55:16 +0800

    "Rendered a static summary statistic for testing latex rendering and ols functionality."

--- src/main.py
+++ src/main.py
@@ -1,10 +1,78 @@
 import flet as ft
 
+import ols
+
 
 def main(page: ft.Page):
     page.title = "Simple Linear Regression"
-    test_run = ft.Text("Hello", size=50)
-    page.add(test_run)
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    x = [1, 2, 3, 4, 5]
+    y = [2, 4, 6, 8, 10]
+    ls = ols.RegressionStatistics()
+    variable_container = ft.Column(
+        width=ft.Window.width,
+        height=ft.Window.height,
+        spacing=10,
+        controls=[
+            ft.Markdown(
+                value=(f"$\\mathbf{{x}} = {x}^{{\\textsf{{T}}}}$"),
+                width=260,
+                selectable=True,
+            ),
+            ft.Markdown(
+                value=(f"$\\mathbf{{y}} = {y}^{{\\textsf{{T}}}}$"),
+                width=260,
+                selectable=True,
+            ),
+            ft.Markdown(
+                value=(
+                    f"$\\bar{{x}} = {ls.sampleMean(x)}$, $\\bar{{y}} = {ls.sampleMean(y)}$"
+                ),
+                width=260,
+                selectable=True,
+            ),
+            ft.Markdown(
+                value=(
+                    f"$\\texttt{{SXX}} = {ls.sumOfSquares(x)}$, $\\texttt{{SYY}} = {ls.sumOfSquares(y)}$"
+                ),
+                width=260,
+                selectable=True,
+            ),
+            ft.Markdown(
+                value=(
+                    f"$\\sigma_{{\\mathbf{{x}}}} = {ls.sampleStandardDeviation(x)}$"
+                ),
+                width=260,
+                selectable=True,
+            ),
+            ft.Markdown(
+                value=(
+                    f"$\\sigma_{{\\mathbf{{y}}}} = {ls.sampleStandardDeviation(y)}$"
+                ),
+                width=260,
+                selectable=True,
+            ),
+            ft.Markdown(
+                value=(f"$\\texttt{{SXY}} = {ls.sumOfCrossProducts(x, y)}$"),
+                width=260,
+                selectable=True,
+            ),
+            ft.Markdown(
+                value=(f"$s_{{\\mathbf{{xy}}}} = {ls.sampleCovariance(x, y)}$"),
+                width=260,
+                selectable=True,
+            ),
+            ft.Markdown(
+                value=(f"$r_{{\\mathbf{{xy}}}} = {ls.sampleCorrelation(x, y)}$"),
+                width=260,
+                selectable=True,
+            ),
+        ],
+    )
+    page.add(
+        variable_container,
+    )
 
 
 if __name__ == "__main__":
